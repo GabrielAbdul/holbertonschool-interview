fa5a8d4b908754fb9a947a0dd70f54496348990f
Gabriel <Gabrielabdul1@gmail.com>
2021-11-05 04:21:39 +0000

    initial commit

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,42 @@
+#include "search.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers.
+ * @list: list
+ * @value: value we're searching for
+ * Return: int
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *c, *p;
+
+	if (!list)
+		return (NULL);
+
+	c = list->express;
+	p = list;
+	/* traverse express lane */
+	while (c)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", c->index, c->n);
+		if (c->n >= value)
+			break;
+		p = c;
+		if (c->express == NULL)
+		{
+			while (c->next)
+				c = c->next;
+			break;
+		}
+		c = c->express;
+	}
+	printf("Value found between indexes [%lu] = [%lu]\n", p->index, c->index);
+	while (p && p->index <= c->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", p->index, p->n);
+		if (p->n == value)
+			return (p);
+		p = p->next;
+	}
+	return (NULL);
+}
---
+++ 0x0E-linear_skip/README.md
@@ -0,0 +1,14 @@
+# 0x0E. Linear search in skip list
+
+## Resources:books:
+Read or watch:
+
+---
+## Learning Objectives:bulb:
+What you should learn from this project:
+
+---
+---
+
+## Author
+* **Gabriel Abdul-Raheem** - [gabrielabdul](https://github.com/gabrielabdul)
