f2dd8e6228e11d91311bef6a07b995ad8dce26c8
Gabriel <Gabrielabdul1@gmail.com>
2021-11-19 14:23:35 +0000

    initial commit

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+calculate how many square units of water will be retained after it rains
+'''
+
+
+def rain(walls):
+    '''calculate how many units of water will be retained after it rains'''
+
+    if walls is []:
+        return 0
+
+    for wall in walls:
+        if wall < 0:
+            return 0
+
+    rain = 0
+    # for ever element in the array
+    for i, num in enumerate(walls):
+        # find max element on left
+        lm = max(walls[:index + 1])
+
+        # find max element on right
+        rm = max(walls[index:])
+        small = min(lm, rm)
+
+        # update max rain water
+        rain += max(small - num, 0)
+
+    return rain
