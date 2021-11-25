3e16d0ed857cef124e9dc03766ff6e7fc6ae1dcc
Gabriel <Gabrielabdul1@gmail.com>
2021-11-25 06:02:31 +0000

    adding necessary files

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -7,5 +7,6 @@
 void heap_sort(int *array, size_t size);
 void utility(int *a, int *b);
 void heapify(int *arr, int n, int i, size_t size);
+void print_array(const int *array, size_t size);
 
 #endif /* SORT_H */
