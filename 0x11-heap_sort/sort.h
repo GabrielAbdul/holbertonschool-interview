5579e3d2d1329026cc458f6b060525b7f5877884
Gabriel <Gabrielabdul1@gmail.com>
2021-11-25 06:00:15 +0000

    initial commit

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void heap_sort(int *array, size_t size);
+void utility(int *a, int *b);
+void heapify(int *arr, int n, int i, size_t size);
+
+#endif /* SORT_H */
