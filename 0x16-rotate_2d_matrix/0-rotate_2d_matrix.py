7e6f11467c76746c46df453e436212d6e1cf5aab
Gabriel <Gabrielabdul1@gmail.com>
2022-01-04 21:08:31 +0000

    rotates a 2D matrix

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+''' rotate 2D matrix module '''
+
+
+def rotate_2d_matrix(matrix):
+        '''function that rotates a 2D matrix 90 degrees clockwise'''
+
+        ''' [::-1] will reverse the list, the zip operater returns an
+            iterator of tuples, and list() converts it back.... python'''
+        rotated = list(zip(*matrix[::-1]))
+        for rows in range(len(matrix)):
+            for cols in range(len(matrix)):
+                matrix[rows][cols] = rotated[rows][cols]
