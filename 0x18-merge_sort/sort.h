bbb828710a6d8c5ff7197bba8c6cdf18d39e15de
Gabriel <Gabrielabdul1@gmail.com>
2022-01-20 03:11:33 +0000

    header file

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_array(const int *array, size_t size);
+/* void merge(int *arr, int start, int mid, int end, int *array); */
+void merge(int *arry, int *arr, int start, int mid, int end);
+void split_merge(int *array, int start, int end, int *arr);
 void merge_sort(int *array, size_t size);
 
 #endif /* SORT_H */
