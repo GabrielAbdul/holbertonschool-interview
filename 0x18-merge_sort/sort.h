8f6db28eb8470fce44247a98fdae7f3c68967167
Gabriel <Gabrielabdul1@gmail.com>
2022-01-20 03:12:59 +0000

    removed unused proto

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 
 void print_array(const int *array, size_t size);
-/* void merge(int *arr, int start, int mid, int end, int *array); */
 void merge(int *arry, int *arr, int start, int mid, int end);
 void split_merge(int *array, int start, int end, int *arr);
 void merge_sort(int *array, size_t size);
