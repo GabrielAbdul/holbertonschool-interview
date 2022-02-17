6d1694db7d41d38dad5661bc9fdebf1b7d9b5d4d
Gabriel <Gabrielabdul1@gmail.com>
2022-02-17 20:34:52 +0000

    accounted for the perimeter of the edges of the grid

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -9,9 +9,12 @@ def island_perimeter(grid):
         grid - a list of list of ints, 1 represents land, 0 water
     '''
     count = 0
+    # iterate throug the grid
     for i, _list in enumerate(grid):
         for j, spot in enumerate(_list):
+            # if were at a 1
             if spot == 1:
+                # count each edge of the spot
                 count += count_edges(grid, i, j)
     return count
 
@@ -23,7 +26,10 @@ def count_edges(grid, i, j):
         i, j - indexes
     '''
     count = 0
+    print("hello:", grid[i - 1][j])
     try:
+        if i - 1 == -1 or j - 1 == -1:
+            count += 1
         if grid[i - 1][j] == 0:
             count += 1
         if grid[i + 1][j] == 0:
@@ -34,5 +40,4 @@ def count_edges(grid, i, j):
             count += 1
     except IndexError:
         count += 1
-
     return count
