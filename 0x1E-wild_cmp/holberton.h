5cf33b4f60eb921709c021b3eab445d1f7c15422
Gabriel <Gabrielabdul1@gmail.com>
2022-03-01 22:27:18 +0000

    header file

---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int wildcmp(char *s1, char *s2);
+int find_index(char *string, char ch, int index);
+
+#endif /* HOLBERTON_H */
