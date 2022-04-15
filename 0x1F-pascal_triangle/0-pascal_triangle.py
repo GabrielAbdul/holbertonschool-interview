eec2586accc9f73cee196d9258c30856dbbdd0e7
Gabriel <Gabrielabdul1@gmail.com>
2022-04-15 04:28:17 +0000

    initial commit -- returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''0-pascal_triangle module'''
+
+
+def pascal_triangle(n):
+	'''retuns a result of integers representing the Pascal triangle'''
+
+	result = []
+
+	if n <= 0:
+		return result
+
+	for i in range(n):
+		result.append([1])
+		for m in range(1, i):
+			result[i].append(result[i - 1][m - 1] + result[i - 1][m])
+		if n != 0 and i != 0:
+			result[i].append(1)
+	return result
